ad58a4c34162261f638780706405fadc9862fb98
linca <lhcfl@outlook.com>
2026-01-05 02:29:37 +0800

    make continous \n

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -121,7 +121,7 @@ export async function markdownToTelegramHtml(
   markdown: string,
 ): Promise<string> {
   const result = await processor.process(markdown);
-  return result.toString();
+  return result.toString().replaceAll(/\n+/, "\n");
 }
 
 const mdparser = unified().use(remarkParse);
