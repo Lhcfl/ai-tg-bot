528b4e60d56b882df0e225dcfdb420556bbbc88e
linca <lhcfl@outlook.com>
2026-01-05 21:52:18 +0800

    refactor: tools

---
+++ src/lib/multiline.ts
@@ -0,0 +1,3 @@
+export function concat(...text: string[]) {
+  return text.join("");
+}
--- src/modules/ai/tools.ts
+++ src/modules/ai/tools.ts
@@ -4,45 +4,8 @@ import z from "zod";
 import type { Context } from "@/lib/bot-proxy";
 import { markdownToTelegramHtml } from "@/lib/markdown";
 
-const EXAMPLE = {
-  kind: "auto_reply",
-  when: "^hello$",
-  message: "Hi there! How can I assist you today?",
-};
-
 export const SYSTEM_PROMPT_DEFAULT = `你是一个接入了 AI 的群聊机器人。
 你会帮助用户回答问题，提供建议，或者进行有趣的对话。`;
-
-export const PROMPT_TOOLS = `
-请确保你的回答简洁明了，避免过长的回复。
-除非使用工具，你没有记忆。
-
-# 执行工具
-
-你可以在输出中使用语言为 "exec" 的代码块，内部写 JSON，来执行系统命令。例如：
-
-\`\`\`exec
-${JSON.stringify(EXAMPLE)}
-\`\`\`
-
-除了 "remember" 以外，只有在用户明确说的时候才应该使用工具。
-*必须* 确保生成的 JSON 格式正确无误。
-
-# 工具类型
-
-- auto_reply: 以后当收到匹配 {when} 正则表达式的消息时，自动回复 {message} 内容。
-  正则表达式可以使用 $1, $2 等来引用捕获组。
-
-- reply_timeout: 在 {timeout} 毫秒后回复一条消息，内容为 {message}。
-
-- remember: 记住一段信息，内容为 {message}。
-
-# 替换
-
-所有 "message" 字段都可以：
-- 使用 $username 来引用发送消息的用户名。
-- 使用 $1, $2 等来引用正则表达式的捕获组（如果有）。
-`;
 
 export const AutoReplySchema = z.object({
   kind: z.literal("auto_reply"),
@@ -51,23 +14,16 @@ export const AutoReplySchema = z.object({
 });
 
 export const ReplyAfterSchema = z.object({
-  kind: z.literal("reply_after"),
   timeout: z.number().min(1),
   message: z.string(),
 });
 
 export const RememberSchema = z.object({
-  kind: z.literal("remember"),
   message: z.string(),
 });
 
-export const aiSchema = z.xor([
-  AutoReplySchema,
-  ReplyAfterSchema,
-  RememberSchema,
-]);
-
 import { createTable } from "@/lib/db";
+import { concat } from "@/lib/multiline";
 
 // 定义表结构
 export const memoriesTable = createTable("memories", {
@@ -110,9 +66,22 @@ function makeTool<T extends Tool>(tool: T): T {
 
 export const makeToolSet = (ctx: Context, msg: TelegramBot.Message) => ({
   auto_reply: makeTool({
-    description:
-      "以后当收到匹配 {when} 正则表达式的消息时，自动回复 {message} 内容。正则表达式可以使用 $1, $2 等等来引用捕获组。",
+    description: concat(
+      "该工具允许 bot 未来当收到匹配 /{when}/i 这一正则表达式的消息时，自动回复 {message} 的内容。",
+      "正则表达式可以使用 $1, $2 等等来引用捕获组。",
+      "只有用户明确要求自动回复时，才应当使用该工具。",
+      `"kind": "auto_reply" 不可以忽略。`,
+    ),
     inputSchema: AutoReplySchema,
+    inputExamples: [
+      {
+        input: {
+          kind: "auto_reply",
+          when: "^你好$",
+          message: "你好！有什么可以帮您的吗？",
+        },
+      },
+    ],
     strict: true,
     async execute(input) {
       await execsTable.insert({
@@ -147,7 +116,7 @@ export const makeToolSet = (ctx: Context, msg: TelegramBot.Message) => ({
     },
   }),
   remember: makeTool({
-    description: "记住一段信息，内容为 {message}。",
+    description: "跨越会话记住一段信息，内容为 {message}。",
     inputSchema: RememberSchema,
     strict: true,
     async execute(input) {
